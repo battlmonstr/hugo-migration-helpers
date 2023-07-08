a24e37d8406993dc322aed782e299a00360c1bc4
battlmonstr <battlmonstr@users.noreply.github.com>
2023-07-08 20:40:44 +0200

    check.sh improvements

--- check.sh
+++ check.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+# build the site content with 2 hugo versions
+# and show files that differ
+
+set -e
+
 repo="$1"
 version1="$2"
 version2="$3"
 
-function generate_clean {
-	rm -rf "public-$1"
-	rm "$repo/hugo"
-}
+scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+siteName=$(basename "$repo")
+siteOutDir="$scriptDir/public/$siteName"
 
 function generate {
-	generate_clean $1
+	version=$1
 	pushd "$repo"
-	ln -s ../releases/$1/hugo hugo
-	out="../public-$1"
-	mkdir "$out"
+	rm -f hugo
+	ln -s "$scriptDir/releases/$version/hugo" hugo
+    out="$siteOutDir/$version"
+    mkdir -p "$out"
 	./build.sh install "$out"
 	popd
 }
 
 function compare {
-	diff -r --ignore-space-change --ignore-all-space --ignore-blank-lines -q "public-$1" "public-$2"
+	diff -r --ignore-space-change --ignore-all-space --ignore-blank-lines -q "$1" "$2"
 }
 
 generate $version1
 generate $version2
-compare $version1 $version2
+compare "$siteOutDir/$version1" "$siteOutDir/$version2"
